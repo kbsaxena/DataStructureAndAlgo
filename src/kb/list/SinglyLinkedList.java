fcb0f5b0fa7005c670aed22fbb3444a79a7e16c6
Kulbhushan Saxena <Kulbhushan.Saxena@thomsonreuters.com>
2018-12-01 16:15:40 +0530

    Added Node Class

--- src/kb/list/SinglyLinkedList.java
+++ src/kb/list/SinglyLinkedList.java
@@ -1,5 +1,15 @@
 package kb.list;
 
 public class SinglyLinkedList {
+     
+}
 
+class Node<K>{
+    K value;
+    Node<K> next;
+    
+    Node(K value){
+        this.value = value;
+        this.next = null;
+    }
 }
